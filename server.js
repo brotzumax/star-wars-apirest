9a13d4ff15740d7612fa214e45f13553af414f2a
Maximiliano <brotzumaxx@gmail.com>
2022-11-09 04:15:07 -0300

    Metodo findWeapon y metodo get

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
+const fs = require('fs');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hola :)");
+function findWeapon(name){
+    const data = JSON.parse(fs.readFileSync("./data/weapons.json"));
+    const weapon = data.find(weapon => weapon.name === name);
+    return weapon;
+}
+
+app.get("/weapons", (req, res) => {
+    const data = fs.readFileSync("./data/weapons.json", "utf-8");
+    res.send(JSON.parse(data));
+});
+
+app.get("/weapon/:nameWeapon", (req, res) => {
+    const nameWeapon = req.params['nameWeapon'];
+    const weapon = findWeapon(nameWeapon);
+    res.send(weapon);
 });
 
 const server = app.listen(8080, () => {
