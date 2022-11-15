f68859771c357a33ce9b53b7a1ef8a15fd41440c
Maximiliano <brotzumaxx@gmail.com>
2022-11-14 23:04:22 -0300

    Last commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 function findObjectByName(fileName, objectName) {
     const data = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
@@ -44,6 +54,8 @@ app.get("/equipment/:nameEquipment", (req, res) => {
     res.send(equipment);
 });
 
+
+//RAZAS
 app.get("/races", (req, res) => {
     const data = fs.readFileSync("./data/races.json", "utf-8");
     res.send(JSON.parse(data));
@@ -52,15 +64,128 @@ app.get("/races", (req, res) => {
 app.get("/races/:nameRace", (req, res) => {
     const nameRace = req.params['nameRace'];
     const race = findObjectByName("races", nameRace);
-    res.send(race);
+    res.json(race);
 });
 
 
+//PROFESIONES
+app.get("/professions", (req, res) => {
+    const data = fs.readFileSync("./data/professions.json", "utf-8");
+    res.send(JSON.parse(data));
+});
+
+app.get("/professions/:nameProfession", (req, res) => {
+    const nameProfession = req.params['nameProfession'];
+    const profession = findObjectByName("professions", nameProfession);
+    res.json(profession);
+});
+
+//INICIAR SESION
+app.post("/iniciar-sesion", (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    const playerList = JSON.parse(fs.readFileSync("./data/players/players.json", "utf-8"));
+
+    for (let player of playerList) {
+        if (player.username === username) {
+            if (player.password === password) {
+                return res.json({ status: "Sesion iniciada", username: username });
+            } else {
+                return res.json({ error: "Contraseña incorrecta" });
+            }
+        }
+    }
+
+    playerList.push({ username: username, password: password });
+    fs.writeFileSync("./data/players/players.json", JSON.stringify(playerList, null, 2));
+    fs.mkdirSync(`./data/players/${username}`);
+    fs.writeFileSync(`./data/players/${username}/characters.json`, "[]");
+    res.json({ status: "Ususario registrado", username: username });
+});
+
+//BUSCAR PERSONAJES POR NOMBRE DE USUARIO
+app.get("/characters/:username", (req, res) => {
+    const username = req.params['username'];
+    const characters = JSON.parse(fs.readFileSync(`./data/players/${username}/characters.json`));
+    res.json(characters);
+});
+
 //CREAR PERSONAJE
-app.post("/character", (req, res) => {
+function getSkillsCharacter(character, professionSkills) {
+    const skills = JSON.parse(fs.readFileSync("./data/skills.json"));
+    const characterSkills = [];
+
+    for (let skill of skills) {
+        skill.profession = professionSkills.includes(skill.name);
+        skill.value = 0;
+        characterSkills.push(skill);
+    }
+
+    return characterSkills;
+}
+
+function getCombatSkillsCharacter(character, professionSkills) {
+    const combatSkills = JSON.parse(fs.readFileSync("./data/combatSkills.json"));
+    const characterCombatSkills = [];
+
+    for (let combatSkill of combatSkills) {
+        combatSkill.profession = professionSkills.includes(combatSkill.name);
+        combatSkill.value = 0;
+        characterCombatSkills.push(combatSkill);
+    }
+
+    return characterCombatSkills;
+}
+
+app.post("/new-character/:username", (req, res) => {
+    const username = req.params['username'];
     const character = req.body;
-    fs.writeFileSync(`./data/characters/${character.characterName}.json`, JSON.stringify(character, null, 2));
-    res.send(character);
+
+    //Se guarda el nombre del personaje en el JSON de la carpeta del jugador
+    const characters = JSON.parse(fs.readFileSync(`./data/players/${username}/characters.json`, "utf-8"));
+    characters.push({ name: character.name });
+    fs.writeFileSync(`./data/players/${username}/characters.json`, JSON.stringify(characters, null, 2));
+
+
+    //Raza y profesion del personaje
+    const race = findObjectByName("races", character.race);
+    const profession = findObjectByName("professions", character.profession);
+
+
+    //Se aplican las características al personaje
+    character.features = race.features;
+
+
+    //Se aplican las habilidades al personaje, siendo true las de profesion
+    character.skills = getSkillsCharacter(character, profession.skills);
+    character.combatSkills = getCombatSkillsCharacter(character, profession.skills);
+
+
+    //Se asignan los rasgos de la raza y profesion
+    console.log(profession.traits);
+    character.traits = race.special.concat(profession.traits);
+
+    //OTROS
+    character.weapons = [];
+    character.equipments = [];
+
+    //Se aplica la protección y heridas al personaje, iniciando sin protección
+    character.armor = findObjectByName("armors", "Sin protección");
+    character.injuryStatement = { maxInjuries: (race.injury + character.features.find(feature => feature.name === "Fortaleza").value), currentInjuries: 0, criticalInjuries: 0 };
+    character.tensionStatement = { maxTension: (race.tension + character.features.find(feature => feature.name === "Voluntad").value), currentTension: 0 };
+
+
+    fs.writeFileSync(`./data/players/${username}/${character.name}.json`, JSON.stringify(character, null, 2));
+    res.json({ status: "Personaje creado" });
+});
+
+//SELECCIONAR PERSONAJE POR NOMBRE Y POR USUARIO
+app.get("/:username/:nameCharacter", (req, res) => {
+    const username = req.params['username'];
+    const nameCharacter = req.params['nameCharacter'];
+
+    const character = JSON.parse(fs.readFileSync(`./data/players/${username}/${nameCharacter}.json`, "utf-8"));
+    res.json(character);
 });
 
 const server = app.listen(8080, () => {
